72ef86d21f5bdf4ae83801ca3a914734e2819ead
Fabio Belavenuto <belavenuto@gmail.com>
2017-05-23 08:24:19 -0300

    New FRS driver, modified for new hardware. Todo tests.

---
+++ _make.sh
@@ -0,0 +1,12 @@
+cd Driver
+sjasmplus --lst=../debug.aux/Driver.prn Driver.asm
+if [ $? -gt 0 ] ;then exit ;fi
+
+cd ../Updater
+sjasmplus --lst=../debug.aux/sdmupd.prn sdmupd.asm
+if [ $? -gt 0 ] ;then exit ;fi
+
+cd ..
+#wine "Nextor\mknexrom" "Nextor\Nextor-2.1-alpha2.base.dat Driver\SDMAPPER.ROM /d:Driver\driver.bin /m:Nextor\Mapper.ASCII16.bin"
+wine Nextor/mknexrom Nextor\\Nextor-2.1-alpha2.base.dat Driver\\SDMAPPER.ROM /d:Driver\\driver.bin /m:Nextor\\Mapper.ASCII16.bin
+
